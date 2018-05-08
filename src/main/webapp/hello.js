a2539c8ce1184656c2b38d36f12af4553d94b4f0
Steven Hampton <steven@stevenhampton.com>
2018-05-08 21:42:53 +1000

    add js file

--- src/main/webapp/hello.js
+++ src/main/webapp/hello.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/BabyLoggerWeb/webresources/com.stevenhampton.babyloggerweb.rest.event"
-    }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
-    });
+    $.ajax(
+                {
+                    type: "GET",
+                    url: 'http://localhost:8080/BabyLoggerWeb/webresources/com.stevenhampton.babyloggerweb.rest.event',
+                    data: "{1}",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (data) {
+
+                        alert('success');
+                        $.each(data, function (i, theItem) {
+                            var combo = document.getElementById("cboFastBikes");
+                            var option = document.createElement("option");
+                            option.text = theItem.toString();
+                            option.value = theItem.toString();
+                            try {
+                                //alert('success add combo');
+                                combo.add(option, null); // Other browsers
+                            }
+                            catch (error) {
+                                alert('error found');
+                                combo.add(option); // really old browser
+                            }
+
+                        });
+                    },
+                    error: function (msg, url, line) {
+                        alert('error trapped in error: function(msg, url, line)');
+                        alert('msg = ' + msg + ', url = ' + url + ', line = ' + line);
+
+                    }
+                });
 });
